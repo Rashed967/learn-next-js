a00a5dd6bfc68f2f25aa7699aad3febee8b6e03c
Rashed967 <rashedhasan967@gmail.com>
2024-11-24 10:25:08 +0600

    just commit fro learn

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana, Noto_Sans_Bengali, Hind_Siliguri} from 'next/font/google'
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: '700', style: 'normal'})
+export const noto_serif_bengali = Noto_Sans_Bengali({subsets: ['bengali'], weight: '400', style: "normal"})
+export const hind_siliguri = Hind_Siliguri({subsets: ['bengali'], weight: '400'})
